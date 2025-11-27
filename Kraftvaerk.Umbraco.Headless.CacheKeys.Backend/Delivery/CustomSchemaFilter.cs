2198079a0a1a731358373d711945805813e2574d
Kaspar Boel Kjeldsen <kaspark88@gmail.com>
2025-11-27 08:24:51 +0100

    v17 release

--- Kraftvaerk.Umbraco.Headless.CacheKeys.Backend/Delivery/CustomSchemaFilter.cs
+++ Kraftvaerk.Umbraco.Headless.CacheKeys.Backend/Delivery/CustomSchemaFilter.cs
@@ -1,14 +1,37 @@
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi;
+#if NET10_0_OR_GREATER
 
+#else
+using Microsoft.OpenApi.Models;
+#endif
+
+using Swashbuckle.AspNetCore.SwaggerGen;
 namespace Kraftvaerk.Umbraco.Headless.CacheKeys.Backend.Delivery;
 
-public class CustomSchemaFilter : IDocumentFilter
+public sealed class CustomSchemaFilter : IDocumentFilter
 {
+
     public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
     {
-        foreach ((string schemaId, OpenApiSchema schema) in context.SchemaRepository.Schemas.Where(x => x.Key.InvariantEndsWith("PropertiesModel")))
+        foreach (var (schemaId, schema) in context.SchemaRepository.Schemas
+                     .Where(x => x.Key.InvariantEndsWith("PropertiesModel")))
         {
+            if(schema.Properties is null)
+            {
+                continue;
+            }
+#if NET10_0_OR_GREATER
+            schema.Properties["cacheKeys"] = new OpenApiSchema
+            {
+                // JsonSchemaType is the new enum for OpenAPI schema types
+                Type = JsonSchemaType.Array,
+                Items = new OpenApiSchema
+                {
+                    Type = JsonSchemaType.String,
+                    Format = "uuid"
+                }
+            };
+#else
             schema.Properties["cacheKeys"] = new OpenApiSchema
             {
                 Type = "array",
@@ -18,7 +41,9 @@ public class CustomSchemaFilter : IDocumentFilter
                     Format = "uuid"
                 }
             };
+#endif
+
         }
+
     }
 }
-
